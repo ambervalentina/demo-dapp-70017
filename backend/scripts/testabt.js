51d8a394c42d79e183304faf8de340e93db04262
Amber 🫐 <ambervalentinag@gmail.com>
2025-01-06 00:56:40 +0800

    Delete backend/scripts/testabt.js

--- backend/scripts/testabt.js
+++
@@ -1,6 +0,0 @@
-// const Cinnamonroll = require('./artifacts/contracts/Cinnamonroll.sol/Cinnamonroll.json');
-const Multicall = require('./artifacts/contracts/Multicall.sol/Multicall.json');
-
-// Log the content to verify
-// console.log(Cinnamonroll);
-console.log(Multicall);
